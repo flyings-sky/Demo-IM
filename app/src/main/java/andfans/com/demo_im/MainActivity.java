54c536b2f02797e189eeda9df8ba786eb787aa68
fly <614015229@qq.com>
2017-05-11 00:35:44 +0800

    基本完成了注册界面和部分注册逻辑的编写

--- app/src/main/java/andfans/com/demo_im/MainActivity.java
+++ app/src/main/java/andfans/com/demo_im/MainActivity.java
@@ -2,13 +2,6 @@ package andfans.com.demo_im;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.avos.avoscloud.AVException;
-import com.avos.avoscloud.AVObject;
-import com.avos.avoscloud.SaveCallback;
-
-import andfans.com.demo_im.Utils.Util;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //测试SDK是否正常工作的代码
-        AVObject testObject = new AVObject("TestObject");
-        testObject.put("words","HelloWorld!");
-        testObject.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(AVException e) {
-                if(e == null){
-                    Log.d(Util.TAG,"success");
-                }
-            }
-        });
     }
 }

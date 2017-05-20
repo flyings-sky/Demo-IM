9c1825db07ba174c5df2dfdefe4cd9c2c9c83bfe
flyings-sky <614015229@qq.com>
2017-05-20 18:01:11 +0800

    配置了Kotlin开发环境，使用Kotlin改写了部分代码

---
+++ app/src/main/java/andfans/com/demo_im/DemoIM.kt
@@ -0,0 +1,44 @@
+package andfans.com.demo_im
+
+import android.app.Application
+import android.util.Log
+
+import com.avos.avoscloud.AVAnalytics
+import com.avos.avoscloud.AVException
+import com.avos.avoscloud.AVInstallation
+import com.avos.avoscloud.AVOSCloud
+import com.avos.avoscloud.SaveCallback
+
+/**
+
+ * Created by 兆鹏 on 2017/5/10.
+ */
+
+class DemoIM : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AVOSCloud.initialize(this, "f3YcCH7JEdaBvWUXlsdQaIPO-gzGzoHsz", "h7YBMSUuU6r7xYlaI6H4gvsQ")
+        // 启用崩溃错误统计
+        AVAnalytics.enableCrashReport(this.applicationContext, true)
+        //开启调试日志
+        AVOSCloud.setDebugLogEnabled(true)
+        //保存本机的Installation
+        AVInstallation.getCurrentInstallation().saveInBackground(object : SaveCallback() {
+            override fun done(e: AVException?) {
+                if (e == null) {
+                    installationId = AVInstallation.getCurrentInstallation().installationId
+                } else {
+                    Log.e("Click", e.message)
+                }
+            }
+        })
+
+        // 设置默认打开的 Activity
+        //PushService.setDefaultPushCallback(mContext, FriendsDetailsActivity.class);
+        //PushService.subscribe(this,"NewFriend",NewFriendActivity.class);
+    }
+
+    companion object {
+        var installationId: String = ""
+    }
+}
